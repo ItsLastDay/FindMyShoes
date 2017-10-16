32810f95488bd1a7d483b9fd82b6de13fbba5db4
Mikhail Koltsov <misha@koltsov.su>
2017-10-16 21:29:46 +0300

    Implement login to GoogleDrive API

--- src/crawler/storage.py
+++ src/crawler/storage.py
@@ -1,6 +1,13 @@
+import httplib2
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage as CredStorage
 
 
-
+# Code parts borrowed from
+# https://developers.google.com/drive/v3/web/quickstart/python
 class Storage:
     """The place where all crawled pages are stored"""
 
@@ -18,7 +25,27 @@ class Storage:
 
         Returns: `Storage` object.
         """
-        pass
+        ret = Storage()
+
+        SCOPES = 'https://www.googleapis.com/auth/drive'
+        APPLICATION_NAME = 'FindMyShoes'
+
+        credential_path = "./api_creds.json"
+        store = CredStorage(credential_path)
+        credentials = store.get()
+
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets('./client_secret.json', SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            credentials = tools.run_flow(flow, store, None)
+            print('Storing credentials to ' + credential_path)
+
+        http = credentials.authorize(httplib2.Http())
+        gdrive_service = discovery.build('drive', 'v3', http=http)
+
+        ret.service = gdrive_service
+
+        return ret
 
     def put_page(self, page_url, page_content):
         """Put page with conent into storage
@@ -39,3 +66,4 @@ class Storage:
         Returns: nothing.
         """
         pass
+
