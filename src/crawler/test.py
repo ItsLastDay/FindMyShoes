e5d538d74fe02f68e8ee50f5a78cf368f39f187c
Andrey Kravtsun <avkravtsun@gmail.com>
2017-10-18 14:05:00 +0300

    Own robots.txt parser.

--- src/crawler/test.py
+++ src/crawler/test.py
@@ -1,19 +1,27 @@
 from robots import RobotsProvider
 
-# domain_url = "http://google.com"
-# pages = [
-#     "https://www.google.com/maps/reserve/partners",
-#     "123"
-# ]
+domain_pages = {
+    "https://respect-shoes.ru": ([
+        ("https://respect-shoes.ru/search/map.php", True),
+        ("https://respect-shoes.ru/obuv_id462073", True)
+    ]),
+    "https://google.com":
+        [
+            ("https://www.google.com/maps/reserve/partners", True),
+            ("https://www.google.com/?hl=", True),
+            ("https://www.google.com/?hl=123", True),
+            ("https://www.google.com/?hl=123&", False),
+            ("https://www.google.com/?hL=123&", False) # /?
+        ]
+}
 
-domain_url = "https://respect-shoes.ru"
-pages = [
-    "https://respect-shoes.ru/search/map.php",
-    "123"
-]
+for domain_url in domain_pages:
+    pages_results = domain_pages[domain_url]
+    for page_url, expected in pages_results:
+        result = RobotsProvider.can_be_crawled(domain_url, page_url)
 
-expected_result = [True, False]
-for page_url, expected in zip(pages, expected_result):
-    result = RobotsProvider.can_be_crawled(domain_url, page_url)
-    print(page_url, result)
-    assert result == expected
+        if result != expected:
+            verdict = "allowed" if expected else "disallowed"
+            print("[FAIL] page {} expected to be {} on domain {}".format(page_url, verdict, domain_url))
+        else:
+            print("[OK]   page {}, domain {}: {}".format(page_url, domain_url, result))
