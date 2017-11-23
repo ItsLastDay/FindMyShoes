f6c775cb529ac6f0372128691e691f337666c5cd
Mikhail Koltsov <misha@koltsov.su>
2017-11-23 21:32:25 +0300

    Add filtering by size

--- src/indexing/searcher.py
+++ src/indexing/searcher.py
@@ -7,6 +7,10 @@ from functools import partialmethod
 import math
 from pathlib import Path
 
+import json
+
+from pymongo import MongoClient
+
 logger = getLogger('searcher')
 
 from index_reader import IndexReader
@@ -24,6 +28,11 @@ class QueryProcessor:
         self._k1 = 2.0
         self._b = 0.75
         self._json_dir = json_dir
+
+        self._db_client = MongoClient('localhost', 27017)
+        # Using data imported by `database_storer.sh`.
+        self._db = self._db_client.findmyshoes
+        self._db_docs = self._db.data
 
     def preprocess(self, query: str) -> [str]:
         """
@@ -53,25 +62,48 @@ class QueryProcessor:
             df = word_entry.get('df')
             idf = math.log2((self._ndocs - df + QueryProcessor.IDF_SMOOTH) / (df + QueryProcessor.IDF_SMOOTH))
             score += idf * fqd * (self._k1 + 1) / (fqd + self._k1 * (1 - self._b + self._b * len(doc_words) / self._avgdl))
-        return score
 
-    def ranged_documents(self, terms: [str]) -> [str, float]:
+        with doc_path.open() as doc:
+            return json.load(doc)['url'], score
+
+    def get_urls_match_size(self, desired_size):
+        # `desired_size` - shoes size, integer between 24 and 50.
+
+        # https://stackoverflow.com/a/18148872/5338270
+        return list(map(lambda x: x['url'], self._db_docs.find({ 'sizes': str(desired_size) })))
+
+    def ranked_documents(self, terms: [str]) -> [str, float]:
         for term in terms:
             if self._index_reader.get_word_entry(term) is None:
                 logger.warning("term {} not found in collection".format(term))
-        match_docs = defaultdict(lambda: [])
+        match_docs = defaultdict(list)
         logger.debug("Searching any match documents")
         for term in terms:
             term_docs = self._index_reader.get_documents(term, False)
             for doc in term_docs:
                 match_docs[doc].append(term)
         logger.debug("Any match documents found: %d" % len(match_docs))
-        document_ranks = [(doc, self.bm25_score(doc, terms)) for doc in match_docs]
+        document_ranks = [self.bm25_score(doc, terms) for doc in match_docs]
         return document_ranks
+
+    def filtered_documents(self, terms, ranked_docs):
+        good_urls = set(map(lambda x: x[0], ranked_docs))
+
+        # Filter by size. If integer between 24 and 50 is found
+        # in query, it is considered the desired shoe size (what else it could be?)
+        for term in terms:
+            try:
+                size = int(term)
+                if 24 <= size <= 50:
+                    good_urls &= set(self.get_urls_match_size(size))
+            except ValueError:
+                pass
+
+        return list(filter(lambda x: x[0] in good_urls, ranked_docs))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
 
     from os.path import join
     from common import default_index_dir, default_json_dir
@@ -84,6 +116,7 @@ if __name__ == '__main__':
 
     with IndexReader(inverted_index_path=args.inverted_index_path, dictionary_path=args.dictionary) as index_reader:
         query_processor = QueryProcessor(index_reader, args.json_dir)
+        logger.debug('shoes with 24 size:{}'.format(query_processor.get_urls_match_size(24)))
         def url_from_json(json_path):
             encoded_path = json_path[:json_path.rfind('##')]
             return encoded_path.replace('__', '/', 1).replace('##', '/')
@@ -92,18 +125,22 @@ if __name__ == '__main__':
             logger.debug("Processing query: " + query_string)
             terms = query_processor.preprocess(query_string)
             logger.debug("Extracted terms: " + str(terms))
-            documents_ranks = query_processor.ranged_documents(terms)
+            documents_ranks = query_processor.ranked_documents(terms)
             logger.debug("Documents found: {}".format(len(documents_ranks)))
-            best_documents_ranks = sorted(documents_ranks, key=lambda dr: dr[1], reverse=True)[:limit]
+            logger.debug("Documents found: {}".format(documents_ranks))
+            filtered_docs = query_processor.filtered_documents(terms, documents_ranks)
+            logger.debug("Documents after filtering: {}".format(len(filtered_docs)))
+            best_documents_ranks = sorted(filtered_docs, key=lambda dr: dr[1], reverse=True)[:limit]
             logger.info("Best options: {}".format(list((url_from_json(doc), rank) for doc, rank in best_documents_ranks)))
             return documents_ranks
 
         # process_query("сочные")
         # process_query("швы")
         # process_query("сочные швы")
-        # while True:
-        #     query_string = input('Введите запрос: ')
-        #     process_query(query_string)
+        process_query('24 балетки')
+        while True:
+            query_string = input('Введите запрос: ')
+            process_query(query_string)
         # process_query("балетки резина кожа олень")
-        # process_query("удобные туфли от удовольствия")
+        process_query("удобные туфли от удовольствия")
         process_query("детские балетки с перфорацией")
